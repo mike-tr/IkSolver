f046002112c57fa5718f45e5bb3c751a23337c7e
Mike <Miket9717@gmail.com>
2020-04-28 00:44:36 +0300

    still need to respect the pole, anyway IkSover is about 5 times faster then FastIkFabric

---
+++ Assets/IKSolver.cs
@@ -0,0 +1,166 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class IKSolver : MonoBehaviour
+{
+    [SerializeField] int chainLength = 3;
+    [SerializeField] Transform target;
+    [SerializeField] Transform pole;
+    // Start is called before the first frame update
+    Transform[] bones;
+    Vector3[] positions;
+    Vector3[] StartDir;
+    float[] bonesLength;
+    float completeLength = 0;
+
+    public int iterations = 10;
+    public float delta = 0.25f;
+
+    public float speed;
+
+    public float snapBackStrength = 1f;
+
+    private Transform Root;
+
+    private void Start() {
+        Init();
+    }
+
+    void Init(){
+        bonesLength = new float[chainLength];
+        bones = new Transform[chainLength + 1];
+        positions = new Vector3[chainLength + 1];
+        StartDir = new Vector3[chainLength + 1];
+
+        Transform current = transform;
+        completeLength = 0;
+
+        Root = transform;
+        for (int i = 0; i <= chainLength; i++)
+        {
+            if(Root == null){
+                break;
+            }
+            Root = Root.parent;
+        }
+
+        for (int i = chainLength; i >= 0; i--)
+        {
+            bones[i] = current;
+            if(i == chainLength){
+                StartDir[i] = (target.position - current.position);
+            }else{
+                StartDir[i] = (bones[i + 1].position - current.position);
+                bonesLength[i] = StartDir[i].magnitude;
+                completeLength += bonesLength[i];
+            }
+            current = current.parent;
+        }
+    }
+
+    // Update is called once per frame
+    private void LateUpdate() {
+        // Stopwatch sw = new Stopwatch();
+        // sw.Start();
+        // for (int i = 0; i < 10000; i++)
+        // {
+        //     Solver();
+        // }
+        // sw.Stop();
+        // print("IKS " + (sw.ElapsedMilliseconds));
+
+        Solver();
+    }
+
+    private void Solver(){
+        if(target == null)
+            return;
+        if(bones.Length - 1 != chainLength){
+            Init();
+        }
+
+        var targetPos = target.position;
+        var rootRot = Root.rotation;
+
+        for (int i = 0; i < chainLength + 1; i++)
+        {       
+            positions[i] = bones[i].position;
+        }
+
+        Vector3 direction = bones[0].position - targetPos;
+        if(direction.sqrMagnitude > completeLength * completeLength){
+            for (int i = 1; i < chainLength + 1; i++)
+            {   
+                positions[i] = positions[i-1] - direction.normalized * bonesLength[i - 1];
+            }
+        }else{
+            //positions[chainLength] = target.position;
+            for (int k = 0; k < iterations; k++)
+            {
+                for (int i = 0; i < chainLength; i++){
+                    positions[i + 1] = Vector3.Lerp(positions[i + 1], 
+                        positions[i] + StartDir[i], snapBackStrength);
+                }
+
+                positions[chainLength] = targetPos;
+                for (int i = chainLength - 1; i > 0; i--)
+                {   
+                    positions[i] = positions[i + 1] + (positions[i] - positions[i + 1]).normalized * bonesLength[i];      
+                }
+
+                for (int i = 1; i < chainLength + 1; i++)
+                {   
+                    direction = (positions[i] - positions[i - 1]).normalized;
+                    positions[i] = positions[i - 1] + direction * bonesLength[i - 1];
+                }
+
+                if((targetPos - positions[chainLength]).sqrMagnitude < delta * delta){
+                    break;
+                }
+            }
+        }
+
+        if(pole){
+            for (int i = 1; i < chainLength; i++)
+            {
+                var dir = (positions[i - 1] - positions[i + 1]).normalized;     
+            }
+        }
+
+        for (int i = 0; i < chainLength + 1; i++)
+        {
+            bones[i].position = positions[i];
+            if(i != chainLength){
+                // bones[i].rotation = Quaternion.FromToRotation(StartDir[i], (positions[i + 1] - positions[i]).normalized)
+                //     * Quaternion.Inverse(startRotations[i]);
+                Vector3 dir = rootRot * Vector3.up;
+                bones[i].rotation = Quaternion.FromToRotation(dir, (positions[i + 1] - positions[i]));
+                //bones[i].rotation = Quaternion.LookRotation(startRotations[i], bones[i].up);
+            }else{
+                bones[i].localRotation = target.rotation;
+            }     
+
+            if(Root){
+                bones[i].rotation *= rootRot;
+            }
+        }
+    }
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        var current = this.transform;
+        for (int i = 0; i < chainLength && current != null && current.parent != null; i++){
+            var scale = Vector3.Distance(current.position, current.parent.position) * 0.1f;
+            Handles.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, 
+                current.parent.position - current.position), new Vector3(scale, 
+                Vector3.Distance(current.parent.position, current.position), scale));
+            Handles.color = Color.green;
+            Handles.DrawWireCube(Vector3.up * 0.5f, Vector3.one);
+            current = current.parent;
+        }
+    }
+    #endif
+}
---
+++ Assets/RenderScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class RenderScene : MonoBehaviour
+{
+    public Material mat;
+    public Shader shader;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        if(mat == null) {
+            mat = new Material(shader);
+        }
+
+
+        // Copy the source Render Texture to the destination,
+        // applying the material along the way.
+        // Debug.Log("tests");
+        Graphics.Blit(src, dest, mat);
+    }
+}
